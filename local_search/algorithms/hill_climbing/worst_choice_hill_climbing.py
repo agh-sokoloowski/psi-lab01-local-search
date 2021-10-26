435983b07a0858d44f50706439f897a925913f16
Piotr Sokołowski <dev@sokoloowski.pl>
2021-10-26 21:28:36 +0200

    Worst choice

--- local_search/algorithms/hill_climbing/worst_choice_hill_climbing.py
+++ local_search/algorithms/hill_climbing/worst_choice_hill_climbing.py
@@ -1,4 +1,5 @@
 from typing import Union
+import numpy as np
 from local_search.algorithms.hill_climbing.hill_climbing import HillClimbing
 from local_search.problems.base.state import State
 from local_search.problems.base.problem import Problem
@@ -13,8 +14,15 @@ class WorstChoiceHillClimbing(HillClimbing):
     """
 
     def _climb_the_hill(self, model: Problem, state: State) -> Union[State, None]:
-        # TODO:
-        # - go trough all the neighbors (_get_neighbours is your friend)
-        # - find the worst improving state (with minimal model.improvement(....) > 0)
-        # return it (or the current state if there is no improving state)!
-        pass
+
+        worst_improvement = {
+            "improvement": np.inf,
+            "neighbour": None
+        }
+
+        for neighbour in self._get_neighbours(model, state):
+            if model.improvement(neighbour, state) < worst_improvement["improvement"]:
+                worst_improvement["improvement"] = model.improvement(neighbour, state)
+                worst_improvement["neighbour"] = neighbour
+
+        return worst_improvement["neighbour"] if worst_improvement["neighbour"] is not None else state
